b934d74b83be80618bd0c4f746341cde2ba94f9b
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-05-22 11:05:13 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBT1TL_4sLw1q7Fw47wdh88avDmaDJi8zw",
+    authDomain: "animal-toys-marketplace.firebaseapp.com",
+    projectId: "animal-toys-marketplace",
+    storageBucket: "animal-toys-marketplace.appspot.com",
+    messagingSenderId: "181356413123",
+    appId: "1:181356413123:web:014a57ecb18651b7f26276"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
