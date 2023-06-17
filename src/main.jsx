a4e258ea87d3adebbe9245bce229a228afaa1193
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-06-17 19:36:41 +0600

    provider setup tanstack provider setup

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./Routes/Routes.jsx";
-// import AuthProvider from "./providers/AuthProvider";
+import AuthProvider from "./providers/AuthProvider";
+import { HelmetProvider } from "react-helmet-async";
+import { QueryClientProvider } from "@tanstack/react-query";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<RouterProvider router={router} />
-	</React.StrictMode>
+	<AuthProvider>
+		<HelmetProvider>
+			<QueryClientProvider client={queryClient}>
+				<div className="max-w-screen-xl mx-auto">
+					<RouterProvider router={router} />
+				</div>
+			</QueryClientProvider>
+		</HelmetProvider>
+	</AuthProvider>
 );
